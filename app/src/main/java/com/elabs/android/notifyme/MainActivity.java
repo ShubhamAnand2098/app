b40aabd0234f13d729837dc302c76d5691bde7dc
shubham7930 <1606305@kiit.ac.in>
2018-07-13 23:53:05 +0530

    hey

--- app/src/main/java/com/elabs/android/notifyme/MainActivity.java
+++ app/src/main/java/com/elabs/android/notifyme/MainActivity.java
@@ -14,7 +14,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView dateView, timeView;
+    TextView dateView, timeView, shubham;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
